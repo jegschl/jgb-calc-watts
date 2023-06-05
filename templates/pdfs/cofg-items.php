5213c90c4f44d2a2cc929140db9b45fb411f8414
Jorge Garrido <jegschl@gmail.com>
2023-06-04 23:44:13 -0400

    plantilla de items (artefactos)

---
+++ templates/pdfs/cofg-items.php
@@ -0,0 +1,6 @@
+            <tr>
+                <td><?= $args['name'] ?></td>
+                <td><?= $args['qty'] ?></td>
+                <td><?= $args['pwr'] ?></td>
+                <td><?= $args['duh'] ?></td>
+            </tr>
