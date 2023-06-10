bd1b6109efde86d0d0876157217895d8169da89f
Jorge Garrido <jegschl@gmail.com>
2023-06-10 19:58:55 -0400

    processing msg tpl

---
+++ templates/processing-mesg.php
@@ -0,0 +1,11 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+?>
+
+<div id="rayssa-processing-msg" style="display:none;"> 
+    <h1>Estamos enviando tu solicitud, agreadecemos tu paciencia.</h1> 
+</div>
