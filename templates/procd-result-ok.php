4230f77a2afa669f33b0d7fea35e98407df1be05
Jorge Garrido <jegschl@gmail.com>
2023-06-10 14:52:05 -0400

    Plantilla de resultado de ejercicio de cálculo

---
+++ templates/procd-result-ok.php
@@ -0,0 +1,9 @@
+<div class="rayssa-calc-result-procd-ok">
+    <div class="msg-wrapper">
+        <p>Procesamos los datos y enviamos un email con los resultados.</p>
+        <p>Adicionalmente puede descargar los resultados en un archivo PDF desde el siguiente enlace:</p>
+        <div class="dwld-link-wrapper">
+            <a class="dwld-link" href="<?= $args['download-link'] ?>">Descargar resultados</a>
+        </div>
+    </div>
+</div>
