cf28a852ea644b23f1ff29618cd845787b69f66f
Jorge Garrido <jegschl@gmail.com>
2023-06-10 16:25:13 -0400

    plantilla de procesamiento ok

--- templates/procd-result-ok.php
+++ templates/procd-result-ok.php
@@ -3,7 +3,12 @@
         <p>Procesamos los datos y enviamos un email con los resultados.</p>
         <p>Adicionalmente puede descargar los resultados en un archivo PDF desde el siguiente enlace:</p>
         <div class="dwld-link-wrapper">
-            <a class="dwld-link" href="<?= $args['download-link'] ?>">Descargar resultados</a>
+            <a class="dwld-link" href="<?= $args['download-url'] ?>">
+                <div class="el-group-wrapper">
+                    <i class="fas fa-file-download"></i>
+                    <span>Descargar resultados</span>
+                </div>
+            </a>
         </div>
     </div>
 </div>
