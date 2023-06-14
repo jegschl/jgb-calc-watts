c3168289f18399f041c257fcc529dcdac5e5ffae
Jorge Garrido <jegschl@gmail.com>
2023-06-14 17:08:22 -0400

    parte datos entrada plantilla ongrid

---
+++ templates/emails/html-cong-input-dt.php
@@ -0,0 +1,21 @@
+<h2>Lista de artefactos</h2>
+    <table class="artifacts">
+
+        <thead>
+            <tr>
+                <td>Potencia del panel</td>
+                <td>Cantidad de paneles</td>
+                <td>Región</td>
+                <td>HSP</td>
+            </tr>
+        </thead>
+
+        <tbody>
+            <tr>
+                <td><?= $args['pwr'] ?> Watts</td>
+                <td><?= $args['qty'] ?> unidades</td>
+                <td><?= $args['region'] ?></td>
+                <td><?= $args['hsp'] ?></td>
+            </tr>
+        </tbody>
+    </table>
