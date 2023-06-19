3c4d25a4095363f8a8f4cd62bb3ad8d6c9f302f7
Jorge Garrido <jegschl@gmail.com>
2023-06-19 06:12:45 -0400

    marka de agua en PDF

--- templates/pdfs/cofg-exercise.php
+++ templates/pdfs/cofg-exercise.php
@@ -1,4 +1,15 @@
+<?php 
+    $img_logo_url = site_url('/wp-content/uploads/2023/04/LogoRayssa.png');
+    //$img_logo_url = '/wp-content/uploads/2023/04/LogoRayssa.png';
+    $img_logo_url = apply_filters('rayssa_pdf_img_logo_url',$img_logo_url);
+
+    $img_water_mark = "/var/www/rayssa/public_html/wp-content/uploads/2023/06/LogoRayssa-blanco-inclinado-50x50-1.png";
+    $img_water_mark = apply_filters('rayssa_pdf_img_watermark_path',$img_water_mark);
+?>
 <style>
+    page{
+        background-color: #086AD821;
+    }
     .logo img{
         width: 120px;
         height: auto;
@@ -67,207 +78,213 @@
         text-align: center;
     }
 
+    .main-container{
+        background-color: #D0E3F9;
+        background-image: url(<?= $img_water_mark ?>);
+        height: 100%;
+    }
+
 </style>
-<?php 
-    $img_logo_url = site_url('/wp-content/uploads/2023/04/LogoRayssa.png');
-    //$img_logo_url = '/wp-content/uploads/2023/04/LogoRayssa.png';
-    $img_logo_url = apply_filters('rayssa_pdf_img_logo_url',$img_logo_url);
-?>
-<page>
-    <div class="logo">
-        <img src="<?=  $img_logo_url ?>">
-    </div>
 
-    <h1>Solicitud de cálculo OffGrid</h1>
+<page backcolor="#D0E3F9"
+    footer="date;time;page"
+>
+    <div class="main-container">
+        <div class="logo">
+            <img src="<?=  $img_logo_url ?>">
+        </div>
 
-    <h2>Datos del solicitante</h2>
-    <div class="contact-data-table-wrapper">
-        <table class="contact-data">
+        <h1>Solicitud de cálculo OffGrid</h1>
+
+        <h2>Datos del solicitante</h2>
+        <div class="contact-data-table-wrapper">
+            <table class="contact-data">
+                <thead>
+                    <tr>
+                        <td>Nombres y apellidos</td>
+                        <td>Email</td>
+                        <td>Nro. telefónico</td>
+                        <td>Financiamiento</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td><strong>{nombresApellidos}</strong></td>
+                        <td><strong>{email}</strong></td>
+                        <td><strong>{telefono}</strong></td>
+                        <td><strong>{financiamiento}</strong></td>
+                    </tr>
+                </tbody>
+                
+            </table>
+        </div>
+
+
+        <h2>Lista de artefactos</h2>
+        <table class="artifacts">
+
             <thead>
                 <tr>
-                    <td>Nombres y apellidos</td>
-                    <td>Email</td>
-                    <td>Nro. telefónico</td>
-                    <td>Financiamiento</td>
+                    <td>Nombre artefacto</td>
+                    <td>Cantidad</td>
+                    <td>Potencia</td>
+                    <td>Tiempo de uso diario</td>
+                    <td>KW/h día</td>
+                    <td>KW/h mes</td>
                 </tr>
             </thead>
+
+            <tbody>
+                <?php foreach($args['artifacts'] as $art){ 
+                    do_action('rayssa_pdf_exercise_item',$art);
+                }
+                ?>
+            </tbody>
+        </table>
+
+        <?php
+
+        $consumoDiario = number_format( $args['daylyConsumtion'], 2, ",","." );
+        $pwrPicoNecsr  = number_format( $args['peakPowerRequired'], 2, ",","." );
+
+        ?>
+
+        <h2>Resultados del cálculo</h2>
+        <table class="results">
             <tbody>
                 <tr>
-                    <td><strong>{nombresApellidos}</strong></td>
-                    <td><strong>{email}</strong></td>
-                    <td><strong>{telefono}</strong></td>
-                    <td><strong>{financiamiento}</strong></td>
+                    <td class="calc-result daylytotalKWatts" >
+                        
+                            <span class="title">Consumo diario:</span>
+                            <span class="value"><?= $consumoDiario ?></span>
+                            <span class="unit">KW/h día</span>
+
+                        
+                    </td>
+                    <td class="calc-result nominalCapacity">
+                        
+                            <span class="title">Capacidad neta:</span>
+                            <span class="value"><?= number_format( $args['nominalCap'], 2, ",","." ) ?></span>
+                            <span class="unit">KWatt</span>
+                        
+                    </td>
                 </tr>
+
+                <tr>
+                    <td class="calc-result bruteCapacity">
+                        
+                            <span class="title">Capacidad bruta:</span>
+                            <span class="value"><?= number_format( $args['bruteCap'], 2, ",","." ) ?></span>
+                            <span class="unit">KW/h día</span>
+                    
+                        
+                    </td>
+                    <td class="calc-result hourly-amperage-container" >
+                        
+                            
+                        <span class="title">12v:</span>
+                        <span class="value"><?= number_format( $args['ha12v'], 2, ",","." ) ?></span>
+                        <span class="unit">ah</span>
+                    
+                        /
+                    
+                        <span class="title">24v:</span>
+                        <span class="value"><?= number_format( $args['ha24v'], 2, ",","." ) ?></span>
+                        <span class="unit">ah</span>
+                    
+                        /
+                    
+                        <span class="title">48v:</span>
+                        <span class="value"><?= number_format( $args['ha48v'], 2, ",","." ) ?></span>
+                        <span class="unit">ah</span>
+                            
+                    
+                    </td>
+                </tr>
+
             </tbody>
-            
+        </table>
+
+        <table class="results-s2">
+            <tbody>
+                <tr>
+                    <td class="calc-result hsp">
+                        
+                            <span class="title">Región:</span>
+                            <span class="value"><?= $args['hsp']['region'] ?></span>
+                            <span class="unit"></span>
+                    
+                        
+                    </td>
+                    <td class="calc-result peak-pwr-req" >
+                        
+                            
+                        <span class="title">Potencia pico necesaria:</span>
+                        <span class="value"><?= number_format( $args['peakPowerRequired'], 2, ",","." ) ?></span>
+                        <span class="unit"></span>
+                            
+                    
+                    </td>
+                </tr>
+
+            </tbody>
+        </table>
+
+        <table class="results-s3">
+            <tbody>
+                <tr>
+                    <td class="calc-result panel">
+                        
+                            <span class="title">Panel 335/340w:</span>
+                            <span class="value"><?= $args['pc335_340w'] ?></span>
+                            <span class="unit">unidades</span>
+                    
+                        
+                    </td>
+                    <td class="calc-result panel" >
+                        
+                            
+                        <span class="title">Panel 445/450w:</span>
+                        <span class="value"><?= $args['pc445_450w'] ?></span>
+                        <span class="unit">unidades</span>
+                            
+                    
+                    </td>
+                </tr>
+
+                <tr>
+                    <td class="calc-result panel">
+                        
+                            <span class="title">Panel 550/560w:</span>
+                            <span class="value"><?= $args['pc550_560w'] ?></span>
+                            <span class="unit">unidades</span>
+                    
+                        
+                    </td>
+                    <td class="calc-result panel" >
+                        
+                            
+                        <span class="title">Panel 600/610w:</span>
+                        <span class="value"><?= $args['pc600_610w'] ?></span>
+                        <span class="unit">unidades</span>
+                            
+                    
+                    </td>
+                </tr>
+
+                <tr>
+                    <td class="calc-result panel" colspan="2">
+                        
+                            <span class="title">Panel 650/660w:</span>
+                            <span class="value"><?= $args['pc650_660w'] ?></span>
+                            <span class="unit">unidades</span>
+                    
+                        
+                    </td>
+                    
+                </tr>
+
+            </tbody>
         </table>
     </div>
-
-
-    <h2>Lista de artefactos</h2>
-    <table class="artifacts">
-
-        <thead>
-            <tr>
-                <td>Nombre artefacto</td>
-                <td>Cantidad</td>
-                <td>Potencia</td>
-                <td>Tiempo de uso diario</td>
-                <td>KW/h día</td>
-                <td>KW/h mes</td>
-            </tr>
-        </thead>
-
-        <tbody>
-            <?php foreach($args['artifacts'] as $art){ 
-                do_action('rayssa_pdf_exercise_item',$art);
-            }
-            ?>
-        </tbody>
-    </table>
-
-    <?php
-
-    $consumoDiario = number_format( $args['daylyConsumtion'], 2, ",","." );
-    $pwrPicoNecsr  = number_format( $args['peakPowerRequired'], 2, ",","." );
-
-    ?>
-
-    <h2>Resultados del cálculo</h2>
-    <table class="results">
-        <tbody>
-            <tr>
-                <td class="calc-result daylytotalKWatts" >
-                    
-                        <span class="title">Consumo diario:</span>
-                        <span class="value"><?= $consumoDiario ?></span>
-                        <span class="unit">KW/h día</span>
-
-                    
-                </td>
-                <td class="calc-result nominalCapacity">
-                    
-                        <span class="title">Capacidad neta:</span>
-                        <span class="value"><?= number_format( $args['nominalCap'], 2, ",","." ) ?></span>
-                        <span class="unit">KWatt</span>
-                    
-                </td>
-            </tr>
-
-            <tr>
-                <td class="calc-result bruteCapacity">
-                    
-                        <span class="title">Capacidad bruta:</span>
-                        <span class="value"><?= number_format( $args['bruteCap'], 2, ",","." ) ?></span>
-                        <span class="unit">KW/h día</span>
-                   
-                    
-                </td>
-                <td class="calc-result hourly-amperage-container" >
-                    
-                        
-                    <span class="title">12v:</span>
-                    <span class="value"><?= number_format( $args['ha12v'], 2, ",","." ) ?></span>
-                    <span class="unit">ah</span>
-                
-                    /
-                
-                    <span class="title">24v:</span>
-                    <span class="value"><?= number_format( $args['ha24v'], 2, ",","." ) ?></span>
-                    <span class="unit">ah</span>
-                
-                    /
-                
-                    <span class="title">48v:</span>
-                    <span class="value"><?= number_format( $args['ha48v'], 2, ",","." ) ?></span>
-                    <span class="unit">ah</span>
-                        
-                  
-                </td>
-            </tr>
-
-        </tbody>
-    </table>
-
-    <table class="results-s2">
-        <tbody>
-            <tr>
-                <td class="calc-result hsp">
-                    
-                        <span class="title">Región:</span>
-                        <span class="value"><?= $args['hsp']['region'] ?></span>
-                        <span class="unit"></span>
-                   
-                    
-                </td>
-                <td class="calc-result peak-pwr-req" >
-                    
-                        
-                    <span class="title">Potencia pico necesaria:</span>
-                    <span class="value"><?= number_format( $args['peakPowerRequired'], 2, ",","." ) ?></span>
-                    <span class="unit"></span>
-                        
-                  
-                </td>
-            </tr>
-
-        </tbody>
-    </table>
-
-    <table class="results-s3">
-        <tbody>
-            <tr>
-                <td class="calc-result panel">
-                    
-                        <span class="title">Panel 335/340w:</span>
-                        <span class="value"><?= $args['pc335_340w'] ?></span>
-                        <span class="unit">unidades</span>
-                   
-                    
-                </td>
-                <td class="calc-result panel" >
-                    
-                        
-                    <span class="title">Panel 445/450w:</span>
-                    <span class="value"><?= $args['pc445_450w'] ?></span>
-                    <span class="unit">unidades</span>
-                        
-                  
-                </td>
-            </tr>
-
-            <tr>
-                <td class="calc-result panel">
-                    
-                        <span class="title">Panel 550/560w:</span>
-                        <span class="value"><?= $args['pc550_560w'] ?></span>
-                        <span class="unit">unidades</span>
-                   
-                    
-                </td>
-                <td class="calc-result panel" >
-                    
-                        
-                    <span class="title">Panel 600/610w:</span>
-                    <span class="value"><?= $args['pc600_610w'] ?></span>
-                    <span class="unit">unidades</span>
-                        
-                  
-                </td>
-            </tr>
-
-            <tr>
-                <td class="calc-result panel" colspan="2">
-                    
-                        <span class="title">Panel 650/660w:</span>
-                        <span class="value"><?= $args['pc650_660w'] ?></span>
-                        <span class="unit">unidades</span>
-                   
-                    
-                </td>
-                
-            </tr>
-
-        </tbody>
-    </table>
 </page>
--- templates/pdfs/cong-exercise.php
+++ templates/pdfs/cong-exercise.php
@@ -1,4 +1,13 @@
+<?php 
+    $img_logo_url = site_url('/wp-content/uploads/2023/04/LogoRayssa.png');
+    //$img_logo_url = '/wp-content/uploads/2023/04/LogoRayssa.png';
+    $img_logo_url = apply_filters('rayssa_pdf_img_logo_url',$img_logo_url);
+
+    $img_water_mark = "/var/www/rayssa/public_html/wp-content/uploads/2023/06/LogoRayssa-blanco-inclinado-50x50-1.png";
+    $img_water_mark = apply_filters('rayssa_pdf_img_watermark_path',$img_water_mark);
+?>
 <style>
+    
     .logo img{
         width: 120px;
         height: auto;
@@ -46,125 +55,133 @@
         text-align: center;
     }
 
+    
+    .main-container{
+        background-color: #D0E3F9;
+        background-image: url(<?= $img_water_mark ?>);
+        height: 100%;
+    }
+
 </style>
-<?php 
-    $img_logo_url = site_url('/wp-content/uploads/2023/04/LogoRayssa.png');
-    //$img_logo_url = '/wp-content/uploads/2023/04/LogoRayssa.png';
-    $img_logo_url = apply_filters('rayssa_pdf_img_logo_url',$img_logo_url);
-?>
-<page>
-    <div class="logo">
-        <img src="<?=  $img_logo_url ?>">
-    </div>
 
-    <h1>Solicitud de cálculo OnGrid</h1>
+<page 
+    backcolor="#D0E3F9"
+    footer="date;time;page"
+>
+    <div class="main-container">
+        <div class="logo">
+            <img src="<?=  $img_logo_url ?>">
+        </div>
 
-    <h2>Datos del solicitante</h2>
-    <div class="contact-data-table-wrapper">
-        <table class="contact-data">
+        <h1>Solicitud de cálculo OnGrid</h1>
+
+        <h2>Datos del solicitante</h2>
+        <div class="contact-data-table-wrapper">
+            <table class="contact-data">
+                <thead>
+                    <tr>
+                        <td>Nombres y apellidos</td>
+                        <td>Email</td>
+                        <td>Nro. telefónico</td>
+                        <td>Financiamiento</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td><strong>{nombresApellidos}</strong></td>
+                        <td><strong>{email}</strong></td>
+                        <td><strong>{telefono}</strong></td>
+                        <td><strong>{financiamiento}</strong></td>
+                    </tr>
+                </tbody>
+                
+            </table>
+        </div>
+
+
+        <h2>Datos de paneles</h2>
+        <table class="panels">
+
             <thead>
                 <tr>
-                    <td>Nombres y apellidos</td>
-                    <td>Email</td>
-                    <td>Nro. telefónico</td>
-                    <td>Financiamiento</td>
+                    <td>Potencia del panel</td>
+                    <td>Cantidad de paneles</td>
+                    <td>Región</td>
+                    <td>HSP</td>
                 </tr>
             </thead>
+
             <tbody>
                 <tr>
-                    <td><strong>{nombresApellidos}</strong></td>
-                    <td><strong>{email}</strong></td>
-                    <td><strong>{telefono}</strong></td>
-                    <td><strong>{financiamiento}</strong></td>
+                    <td><strong><?= $args['pwr'] ?></strong></td>
+                    <td><strong><?= $args['qty'] ?> unidades</strong></td>
+                    <td><strong><?= $args['region'] ?></strong></td>
+                    <td><strong><?= $args['hsp'] ?></strong></td>
                 </tr>
             </tbody>
-            
         </table>
-    </div>
 
+        <?php
 
-    <h2>Datos de paneles</h2>
-    <table class="panels">
+        $whbd   = number_format( $args['whbd'], 2, ",","." );
+        $kwhbd  = number_format( $args['kwhbd'], 2, ",","." );
+        $kwhbm  = number_format( $args['kwhbm'], 2, ",","." );
+        $discm  = number_format( $args['discbm'], 0, ",","." );
+        $discy  = number_format( $args['discby'], 0, ",","." );
 
-        <thead>
-            <tr>
-                <td>Potencia del panel</td>
-                <td>Cantidad de paneles</td>
-                <td>Región</td>
-                <td>HSP</td>
-            </tr>
-        </thead>
+        $anchoCRCol = "350";
 
-        <tbody>
-            <tr>
-                <td><strong><?= $args['pwr'] ?></strong></td>
-                <td><strong><?= $args['qty'] ?> unidades</strong></td>
-                <td><strong><?= $args['region'] ?></strong></td>
-                <td><strong><?= $args['hsp'] ?></strong></td>
-            </tr>
-        </tbody>
-    </table>
+        ?>
 
-    <?php
+        <h2>Resultados del cálculo</h2>
+        <table class="results">
+            <tbody>
+                <tr>
+                    <td class="calc-result wh-by-day" >
+                        
+                            <span class="title">W/H Día:</span>
+                            <span class="value"><?= $whbd ?></span>
+                            <span class="unit">W/h</span>
 
-    $whbd   = number_format( $args['whbd'], 2, ",","." );
-    $kwhbd  = number_format( $args['kwhbd'], 2, ",","." );
-    $kwhbm  = number_format( $args['kwhbm'], 2, ",","." );
-    $discm  = number_format( $args['discbm'], 0, ",","." );
-    $discy  = number_format( $args['discby'], 0, ",","." );
+                        
+                    </td>
+                    <td class="calc-result kwh-by-dy">
+                        
+                            <span class="title">KW/H al día:</span>
+                            <span class="value"><?= $kwhbd ?></span>
+                            <span class="unit">KW/H</span>
+                        
+                    </td>
+                </tr>
 
-    $anchoCRCol = "350";
-
-    ?>
-
-    <h2>Resultados del cálculo</h2>
-    <table class="results">
-        <tbody>
-            <tr>
-                <td class="calc-result wh-by-day" >
-                    
-                        <span class="title">W/H Día:</span>
-                        <span class="value"><?= $whbd ?></span>
-                        <span class="unit">W/h</span>
-
-                    
-                </td>
-                <td class="calc-result kwh-by-dy">
-                    
-                        <span class="title">KW/H al día:</span>
-                        <span class="value"><?= $kwhbd ?></span>
-                        <span class="unit">KW/H</span>
-                    
-                </td>
-            </tr>
-
-            <tr>
-                <td class="calc-result kwh-by-month">
-                    
-                        <span class="title">KW/H al mes:</span>
-                        <span class="value"><?= $kwhbm ?></span>
-                        <span class="unit">KW/h</span>
-                   
-                    
-                </td>
-                <td class="calc-result discounts">
+                <tr>
+                    <td class="calc-result kwh-by-month">
+                        
+                            <span class="title">KW/H al mes:</span>
+                            <span class="value"><?= $kwhbm ?></span>
+                            <span class="unit">KW/h</span>
                     
                         
-                    <span class="title">Descuento mensual:</span>
-                    <span class="unit">$</span>
-                    <span class="value"><?= $discm ?></span>
-                    
-                    <br>
-                
-                    <span class="title">Descuento anual:</span>
-                    <span class="unit">$</span>
-                    <span class="value"><?= $discy ?></span>
+                    </td>
+                    <td class="calc-result discounts">
+                        
                             
-                 
-                  
-                </td>
-            </tr>
+                        <span class="title">Descuento mensual:</span>
+                        <span class="unit">$</span>
+                        <span class="value"><?= $discm ?></span>
+                        
+                        <br>
+                    
+                        <span class="title">Descuento anual:</span>
+                        <span class="unit">$</span>
+                        <span class="value"><?= $discy ?></span>
+                                
+                    
+                    
+                    </td>
+                </tr>
 
-        </tbody>
-    </table>
+            </tbody>
+        </table>
+    </div>
 </page>
